60ee1c1d9ffc727444ab3d7a20c4f90961838149
1Doomdie1 <horia.todoran@yahoo.com>
2024-11-14 14:26:04 +0000

    added types

---
+++ core/commands/types.py
@@ -0,0 +1,14 @@
+from enum import StrEnum, IntEnum
+
+class keep_events_for_type(StrEnum):
+    _1h   = "1h"
+    _6h   = "6h"
+    _1d   = "1d"
+    _3d   = "3d"
+    _7d   = "7d"
+    _14d  = "15d"
+    _30d  = "30d"
+    _60d  = "60d"
+    _90d  = "90d"
+    _180d = "180d"
+    _365d = "365d"
