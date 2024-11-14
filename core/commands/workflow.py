603de42c06fb98728e8c23b888f18a11643deeb4
1Doomdie1 <horia.todoran@yahoo.com>
2024-11-14 14:27:49 +0000

    added workflow creation command

--- core/commands/workflow.py
+++ core/commands/workflow.py
@@ -1,16 +1,23 @@
-from typing_extensions import Annotated
-from typer             import Typer, Argument, Option
+from core.commands.types            import *
+from typing_extensions              import Annotated
+from core.managers.workflow_manager import WorkflowManager
+from typer                          import Typer, Argument, Option
 
 app = Typer()
 
 @app.command(help="Create workflow")
 def create(
-    name:        Annotated[str, Argument(..., help="Workflow name"       )],
-    description: Annotated[str, Option  (..., help="Workflow description")] = "Created with tines_cli",
-    location:    Annotated[int, Option  (..., help="Folder id"           )] = None
+    name:            Annotated[str,                  Argument(..., help="The story name"                                                                )],
+    team_id:         Annotated[int,                  Option  (..., help="ID of team to which the story should be added"                                 )],
+    description:     Annotated[str,                  Option  (..., help="A user-defined description of the story"                                       )] = "Created with tines_cli",
+    keep_events_for: Annotated[keep_events_for_type, Option  (..., help="Event retention period"                                                        )] = keep_events_for_type._1d,
+    folder_id:       Annotated[int | None,           Option  (..., help="ID of folder to which the story should be added"                               )] = None,
+    tags:            Annotated[str,                  Option  (..., help="An array of Strings separated by ',' used to create tags to classify the story")] = "",
+    disabled:        Annotated[bool,                 Option  (..., help="Boolean flag indicating whether the story is disabled"                         )] = False,
+    priority:        Annotated[bool,                 Option  (..., help="Boolean flag indicating if this is a high priority story"                      )] = False
 ):
+    WorkflowManager.create_workflow(team_id, name, description, keep_events_for, folder_id, tags, disabled, priority)
 
-    ...
 @app.command(help="Get workflow logs")
 def logs(
     id:      Annotated[int, Argument(..., help="Workflow ID")],
