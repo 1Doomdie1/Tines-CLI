7ed4b59766b0b754c51d0f5a1be921a057fb9369
Todoran <horia.todoran@tesco.com>
2025-04-09 14:16:43 +0100

    made calback function to handle global functionality, in this case disabling SSL

--- tines_cli/main.py
+++ tines_cli/main.py
@@ -1,7 +1,10 @@
 from typer             import Typer
+from os                import getenv
+from dotenv            import load_dotenv
 from tines_cli         import __version__
 from .commands.tenant  import tenant_typer
 from .commands.envvars import envvars_typer
+from tapi.utils.http   import disable_ssl_verification
 
 
 typer = Typer(add_completion = False)
@@ -11,6 +14,15 @@ typer.add_typer(envvars_typer)
 @typer.command(help = "Tool version")
 def version():
     print(__version__)
+
+@typer.callback()
+def callback(invoke_without_command = True):
+    load_dotenv()
+
+    DISABLE_SSL = getenv("DISABLE_SSL")
+
+    if DISABLE_SSL == "1":
+        disable_ssl_verification()
 
 def main():
     typer()
