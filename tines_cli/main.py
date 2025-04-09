6d3bac8a47cacc6f9617f848cdd6932ac25758d0
Todoran <horia.todoran@tesco.com>
2025-04-09 13:26:48 +0100

    added version command

--- tines_cli/main.py
+++ tines_cli/main.py
@@ -1,10 +1,16 @@
 from typer             import Typer
+from tines_cli         import __version__
 from .commands.tenant  import tenant_typer
 from .commands.envvars import envvars_typer
 
-def main():
-    typer = Typer(add_completion = False)
-    typer.add_typer(tenant_typer)
-    typer.add_typer(envvars_typer)
 
+typer = Typer(add_completion = False)
+typer.add_typer(tenant_typer)
+typer.add_typer(envvars_typer)
+
+@typer.command()
+def version():
+    print(__version__)
+
+def main():
     typer()
